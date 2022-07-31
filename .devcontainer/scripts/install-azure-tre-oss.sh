865b9f1908dd77a8282bf0121c412f19facc0f35
Liza Shakury <lishakur@Lizas-MacBook-Pro.local>
2022-07-31 15:17:58 +0300

    CR changes

--- .devcontainer/scripts/install-azure-tre-oss.sh
+++ .devcontainer/scripts/install-azure-tre-oss.sh
@@ -16,28 +16,3 @@ tar -xzf "$archive" -C "$oss_home" --strip-components=1
 rm "$archive"
 
 echo "${oss_version}" > "$oss_home/version.txt"
-
-
-########################################################################
-############ Using this to make temp corrections to the OSS ############
-########################################################################
-
-# waiting for: https://github.com/microsoft/AzureTRE/issues/2156
-# shellcheck disable=SC2016
-sed -i 's,\./devops/scripts/check_dependencies.sh,${MAKEFILE_DIR}/devops/scripts/check_dependencies.sh,g' "$oss_home"/Makefile
-
-# waiting for: https://github.com/microsoft/AzureTRE/issues/2116
-sed -i 's,curl -i -X,curl -i -k -X,g' "$oss_home"/devops/scripts/migrate_state_store.sh
-
-
-# shellcheck disable=SC2016
-sed -i 's,\./ui,${AZURETRE_HOME}/ui,g' "$oss_home"/devops/scripts/build_deploy_ui.sh
-# shellcheck disable=SC2016
-sed -i 's,\./devops,${AZURETRE_HOME}/devops,g' "$oss_home"/devops/scripts/build_deploy_ui.sh
-
-sed -i 's,masked_resource.properties,# masked_resource.properties,g' "$oss_home"/api_app/api/routes/resource_helpers.py
-echo "__version__ = \"0.3.23.2\"" > "$oss_home"/api_app/_version.py
-
-
-# waiting for: https://github.com/microsoft/AzureTRE/pull/2248
-sed -i 's/lstrip('\''api:\/\/'\'')/replace('\''api:\/\/'\'','\'''\'')/g' "$oss_home"/api_app/api/routes/api.py
