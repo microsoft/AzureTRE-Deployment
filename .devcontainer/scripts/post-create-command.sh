851be9f0194c83812d62c86a6e86ea6c7ca5eede
Liza Shakury <42377481+LizaShak@users.noreply.github.com>
2022-08-17 11:01:44 +0300

    Download sample env files in devcontainer. (#3)
    Co-authored-by: Liza Shakury <lishakur@Lizas-MacBook-Pro.local>

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -5,6 +5,10 @@ set -o nounset
 # Uncomment this line to see each command for debugging (careful: this will show secrets!)
 # set -o xtrace
 
+#Copy sample env files
+find AzureTRE/devops/ -name \*.env.sample -exec cp {} devops/ \;
+find AzureTRE/templates/core/ -name \*.env.sample -exec cp {} templates/core/ \;
+
 # show the AzureTRE OSS folder inside the workspace one
 rm -fr AzureTRE || true
 ln -s "${AZURETRE_HOME}" AzureTRE
