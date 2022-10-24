eadf12b8ed4d9048e8b078080da2243659e73527
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-10-24 11:04:03 +0300

    AzureTRE 0.6.0 release (#30)
    * sync to v0.6.0 release
    
    * porter changes
    
    * v0.6.0
    
    * small updates

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# set -o xtrace
+
+# show the AzureTRE OSS folder inside the workspace one
+rm -fr AzureTRE || true
+ln -s "${AZURETRE_HOME}" AzureTRE
+
+# Copy sample env files
+find ~/AzureTRE/devops/ -name \*.env.sample -exec cp {} devops/ \;
+find ~/AzureTRE/templates/core/ -name \*.env.sample -exec cp {} templates/core/ \;
+
+# docker socket fixup
+sudo bash AzureTRE/devops/scripts/set_docker_sock_permission.sh
