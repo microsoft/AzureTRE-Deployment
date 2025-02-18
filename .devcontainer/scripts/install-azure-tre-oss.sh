0e95d1a5a794f3b7244819233ad3dca541f95d85
james-annages <45330941+james-annages@users.noreply.github.com>
2025-02-18 08:48:49 +0000

    Update install-azure-tre-oss.sh

--- .devcontainer/scripts/install-azure-tre-oss.sh
+++ .devcontainer/scripts/install-azure-tre-oss.sh
@@ -5,7 +5,7 @@ set -o nounset
 # Uncomment this line to see each command for debugging (careful: this will show secrets!)
 # set -o xtrace
 
-upstream_repo_url="$1"
+upstream_repo="$1"
 upstream_repo_version="$2"
 upstream_home="$3"
 archive=/tmp/AzureTRE.tar.gz
@@ -16,17 +16,17 @@ then
     TOKEN="$4"
     echo "Using Auth"
     curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/$upstream_repo_url/tarball/$upstream_repo_version --output "$archive"
+    https://api.github.com/repos/$upstream_repo/tarball/$upstream_repo_version --output "$archive"
 
 else
-    wget -O "$archive" "http://github.com/${oss_repo}/archive/${oss_version}.tar.gz" --progress=dot:giga
+    wget -O "$archive" "http://github.com/${upstream_repo}/archive/${upstream_repo_version}.tar.gz" --progress=dot:giga
 fi
 
 mkdir -p "$upstream_home"
 tar -xzf "$archive" -C "$upstream_home" --strip-components=1
 rm "$archive"
 
-echo "${upstream_repo_url}" > "$upstream_home/repository.txt"
+echo "${upstream_repo}" > "$upstream_home/repository.txt"
 echo "${upstream_repo_version}" > "$upstream_home/version.txt"
 
 
