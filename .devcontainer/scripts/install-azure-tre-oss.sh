7df45bb43d53db8b5198329441d31e026ac4523c
Yuval Yaron <yuvalyaron@microsoft.com>
2025-02-26 15:20:49 +0200

    refactor: update installation script to use upstream repository variables

--- .devcontainer/scripts/install-azure-tre-oss.sh
+++ .devcontainer/scripts/install-azure-tre-oss.sh
@@ -6,16 +6,16 @@ set -o nounset
 # set -o xtrace
 
 
-oss_repo="$1"
-oss_version="$2"
-oss_home="$3"
+upstream_repo_url="$1"
+upstream_repo_version="$2"
+upstream_home="$3"
 archive=/tmp/AzureTRE.tar.gz
 
-wget -O "$archive" "http://github.com/${oss_repo}/archive/${oss_version}.tar.gz" --progress=dot:giga
+wget -O "$archive" "${upstream_repo_url}/archive/${upstream_repo_version}.tar.gz" --progress=dot:giga
 
-mkdir -p "$oss_home"
-tar -xzf "$archive" -C "$oss_home" --strip-components=1
+mkdir -p "$upstream_home"
+tar -xzf "$archive" -C "$upstream_home" --strip-components=1
 rm "$archive"
 
-echo "${oss_repo}" > "$oss_home/repository.txt"
-echo "${oss_version}" > "$oss_home/version.txt"
+echo "${upstream_repo_url}" > "$upstream_home/repository.txt"
+echo "${upstream_repo_version}" > "$upstream_home/version.txt"
